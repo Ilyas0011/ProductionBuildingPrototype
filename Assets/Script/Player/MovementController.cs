c7eeb466d42ee61309c2d2c9571ff58dce40d8a1
Ilyas0011 <78249785+Ilyas0011@users.noreply.github.com>
2025-03-26 16:49:34 +0300

    add animation controller

--- Assets/Script/Player/MovementController.cs
+++ Assets/Script/Player/MovementController.cs
@@ -4,11 +4,13 @@ using UnityEngine.AI;
 public class MovementController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private AnimationController _animationContoller;
     private Camera _camera;
 
     private void Awake()
     {
        agent = GetComponent<NavMeshAgent>();
+        _animationContoller = GetComponent<AnimationController>();
        _camera = Camera.main;
     }
 
@@ -18,6 +20,16 @@ public class MovementController : MonoBehaviour
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit))
+        {
             agent.SetDestination(hit.point);
+        }
+    }
+
+    private void Update()
+    {
+        if (agent.velocity.sqrMagnitude == 0f)
+            _animationContoller.PlayWalking(false);
+        else
+            _animationContoller.PlayWalking(true);
     }
 }
